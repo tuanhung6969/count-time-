3130a0ff6234b6aefc18a2bc2c59bf7991515fd0
TuanHung <th1@th.localdomain>
2022-06-19 21:54:09 +0700

    Push first time

---
+++ a.py
@@ -0,0 +1,21 @@
+print("Lưu ý năm đầu tiên phải nhỏ hơn năm thứ 2")
+a = int(input("tháng đầu tiên là "))
+b = int(input("năm đầu tiên là "))
+c = int(input("tháng thứ 2 là "))
+d = int(input("năm thứ 2 là "))
+
+if b > d:
+    print("lỗi")
+elif b <= d:
+    if d - b ==1 :
+        a = 12 - a 
+        f = a + c
+    if d - b ==0:
+        if c - a == 0:
+            f == 0 
+    if d - b >0:
+        a = 12 - a
+        s = d - b -1 
+        s = s*12
+        f = a + c + s
+print("Khoảng cách giữa cột mốc thời gian là " + str(f) + "" + "tháng.")
